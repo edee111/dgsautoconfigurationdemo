00fb570fd8eb7a202bc35ee19dad841e1b1ebccc
Eduard Tomek <eduard.tomek@usu.com>
2024-10-11 11:20:52 +0200

    Spring initializr commit

---
+++ src/main/kotlin/com/example/dgsautoconfigurationdemo/DgsautoconfigurationdemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.dgsautoconfigurationdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DgsautoconfigurationdemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<DgsautoconfigurationdemoApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/dgsautoconfigurationdemo/DgsautoconfigurationdemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.dgsautoconfigurationdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DgsautoconfigurationdemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
